6830a4871399b60f2e0f01e60b2d494aaebe2eba
Amin Yahyaabaadi <aminyahyaabadi74@gmail.com>
2020-12-21 03:33:20 -0600

    Add some basic types

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,30 @@
 import { createState, createRoot, produce } from "solid-js";
 
+
+type EtchSolidElement = any
+interface EtchSolidComponent {
+  element: EtchSolidElement
+  render: () => any
+  update: (...args: any[]) => void
+  destroy: () => void
+  __proto__: any
+}
+
 // etch.initialize
-export function initialize(component) {
+export function initialize(component: EtchSolidComponent) {
 
   const $originUpdate = component.update,
     $originDestroy = component.destroy;
 
   // handle the changes in the states
-  let dispose;
+  let dispose: () => void;
   const d = Object.getOwnPropertyDescriptors(component.__proto__);
   const [$state, setState] = createState(
     Object.assign(Object.create(Object.prototype, d), component)
   );
 
   // handle updating
-  function update(...args) {
+  function update(...args: any[]) {
     setState(produce((s) => $originUpdate.call(s, ...args)));
   }
 
